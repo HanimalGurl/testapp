1c9f64ab640abb4d0490ae183a3058498b384c0e
HanimalGurl <58697416+HanimalGurl@users.noreply.github.com>
2021-11-09 23:00:55 +0000

    Added Relationship methods to Animal and EC

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 {
     use HasFactory;
+
+    public function emergencyContact()
+    {
+        return $this->hasOne(EmergencyContact::class);
+    }
 }
